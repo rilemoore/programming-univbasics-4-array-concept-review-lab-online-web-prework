970ccc66b4a633f2533600c2ef66c840efb00ae8
Riley Moore <rilemoore@gmail.com>
2019-09-19 11:58:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,12 @@
 def find_element_index(array, value_to_find)
   counter = 0
-  while 
+  while array[counter] do
+    if(array[counter] == value_to_find)
+      {
+        return counter
+      }
+      counter += 1
+  end
   # Add your solution here
 end
 
